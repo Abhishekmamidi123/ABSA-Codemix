6b2244ffffb442e733c8ad1b570a9244de8d1d4c
Abhishekmamidi123 <abhishek.m15@iiits.in>
2017-12-22 12:52:14 +0530

    Preprocess Data

---
+++ preprocess.py
@@ -0,0 +1,19 @@
+import json
+
+f = open('data/tagged_text.txt','w')
+
+
+def readData(file_name):
+	global data
+	with open(file_name) as json_data:
+		data = json.load(json_data)
+
+def extractTaggedText():
+	for tweet in data:
+		print tweet['lang_tagged_text']
+		f.write(tweet['lang_tagged_text'].encode('utf-8'))
+		f.write('\n')
+
+file_name = 'data/train_data.json'
+readData(file_name)
+extractTaggedText()
