2305284c8e89caf254b8f8dd099b2d79b69576f9
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-02-26 16:54:39 +0530

    CRF_custom_tagger

--- CustomPOSTagger/CRFTagger_custom.py
+++ CustomPOSTagger/CRFTagger_custom.py
@@ -1,18 +1,15 @@
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-import numpy as np
-np.set_printoptions(threshold=np.nan)
-from xml.etree import cElementTree as ET
-from nltk.tag import CRFTagger
-from random import shuffle
-import pycrfsuite
 import io
+import pycrfsuite
+import numpy as np
+from random import shuffle
+from nltk.tag import CRFTagger
+from xml.etree import cElementTree as ET
+np.set_printoptions(threshold=np.nan)
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 def word2features(doc, i):
     word = doc[i][0]
-    # postag = doc[i][1]
-
-    # Common features for all words
     features = [
         'bias',
         'word.lower=' + word.lower(),
@@ -20,50 +17,37 @@ def word2features(doc, i):
         'word[-2:]=' + word[-2:],
         'word.isupper=%s' % word.isupper(),
         'word.istitle=%s' % word.istitle(),
-        'word.isdigit=%s' % word.isdigit(),
-        # 'postag=' + postag
+        'word.isdigit=%s' % word.isdigit()
     ]
-
-    # Features for words that are not
-    # at the beginning of a document
     if i > 0:
         word1 = doc[i-1][0]
-        #postag1 = doc[i-1][1]
         features.extend([
             '-1:word.lower=' + word1.lower(),
             '-1:word.istitle=%s' % word1.istitle(),
             '-1:word.isupper=%s' % word1.isupper(),
             '-1:word.isdigit=%s' % word1.isdigit()
-    #        '-1:postag=' + postag1
         ])
     else:
-        # Indicate that it is the 'beginning of a document'
         features.append('BOS')
-
-    # Features for words that are not
-    # at the end of a document
     if i < len(doc)-1:
         word1 = doc[i+1][0]
-        #postag1 = doc[i+1][1]
         features.extend([
             '+1:word.lower=' + word1.lower(),
             '+1:word.istitle=%s' % word1.istitle(),
             '+1:word.isupper=%s' % word1.isupper(),
             '+1:word.isdigit=%s' % word1.isdigit()
-#            '+1:postag=' + postag1
         ])
     else:
-        # Indicate that it is the 'end of a document'
         features.append('EOS')
-
     return features
-    
+
 def extract_features(doc):
     return [word2features(doc, i) for i in range(len(doc))]
 
 def get_labels(doc):
-    return [postag for (token, postag) in doc]
+    return [label for (token, label) in doc]
 
+# Main function
 root = ET.fromstring(io.open('Hi-En_data.xml',encoding='utf-8').read())
 count = 0
 data = []
@@ -79,64 +63,53 @@ for page in list(root):
     data.append(l)
     count = count + 1
 shuffle(data)
-#print data
-
-# eightyPercent = count*0.9
-# training_set = data[0:int(eightyPercent)]
-# test_set = data[int(eightyPercent):]
 
 X = [extract_features(doc) for doc in data]
 y = [get_labels(doc) for doc in data]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-print len(X), len(X_train), len(X_test)
 
-trainer = pycrfsuite.Trainer(verbose=False)
+trainer = pycrfsuite.Trainer(verbose=True)
 
-# Submit training data to the trainer
 for xseq, yseq in zip(X_train, y_train):
     trainer.append(xseq, yseq)
-    
+
 trainer.set_params({
     'c1': 0.1,
     'c2': 0.01,  
     'max_iterations': 200,
     'feature.possible_transitions': True
 })
+
 trainer.train('crf.model')
 
 tagger = pycrfsuite.Tagger()
 tagger.open('crf.model')
-#y_pred = [tagger.tag(xseq) for xseq in X_test]
-for i in X_test:
-	print tagger.tag(i)
-print "-=------------------"
-print type(i)
-print tagger.tag(u"mera bharat mahan")
+y_pred = [tagger.tag(xseq) for xseq in X_test]
 
-i = 12
-#for x, y in zip(y_pred[i], [x[1].split("=")[1] for x in X_test[i]]):
-    #print("%s (%s)" % (y, x))
 
-# Create a mapping of labels to indices
+
+i = 3
+for x, y in zip(y_pred[i], [x[1].split("=")[1] for x in X_test[i]]):
+    print("%s (%s)" % (y, x))
+    
 labels = {"G_PRT": 0, "G_X": 1, "E": 2, "PSP": 3, "DT": 4, "G_N": 5, "U": 6, "CC": 7, "G_V": 8, "G_J": 9, "@": 10, "G_SYM": 11, "G_R": 12, "$": 13, "G_PRP": 14, "#": 15, "~": 16, "null": 17}
 
-# labels = {"N": 1, "I": 0}
-
-# Convert the sequences of tags into a 1-dimensional array
+# 
 predictions = np.array([labels[tag] for row in y_pred for tag in row])
 truths = np.array([labels[tag] for row in y_test for tag in row])
-print predictions
-print truths
-if np.array_equal(predictions,truths):
-	print "L"
-else:
-	print "K"
 
-test_sent = raw_input()
-test_sent = test_sent.encode('utf-8').decode('utf-8').split(' ')
-# print test_sent
-print ct.tag_sents([test_sent])
+count = 0
+accuracy = 0
+for i in range(len(predictions)):
+	if predictions[i] == truths[i]:
+		accuracy+=1
+	count+=1
+accuracy = accuracy/float(count)
+print accuracy
 
 # Print out the classification report
 # print(classification_report(truths, predictions, target_names=["G_PRT", "G_X", "E", "PSP", "DT", "G_N", "U", "CC", "G_V", "G_J", "@", "G_SYM", "G_R", "$", "G_PRP", "#", "~", "null"]))
+
+# if np.array_equal(predictions,truths):
+#	print "Equal"
---
+++ CustomPOSTagger/README.md
@@ -0,0 +1,7 @@
+- This directory contains different methods for tagging sentences with parts of speech(POS) on Codemix data.
+- The data is in the file "Hi-En_data.xml"
+
+- Below are the resuts we observed using different methods:
+1. Conditional Random Fields(CRF): 77.185%
+2. Trigram + Bigram + Unigram: 68.172%
+3. Hidden Markov Model(HMM): 18.014
