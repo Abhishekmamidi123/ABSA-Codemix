920e4c608e1e8379ce40576b82e794113ceb18b5
Vagdevi Kommineni <30801221+vagdevik@users.noreply.github.com>
2018-04-06 21:15:52 +0530

    Preprocessing

--- Aspect_Identification/Aspect_LSTM/preprocessing.py
+++ Aspect_Identification/Aspect_LSTM/preprocessing.py
@@ -1,5 +1,6 @@
 import json
 import string
+import validators
 from nltk.corpus import stopwords
 exclude = set(string.punctuation)
 english_stopwords = set(stopwords.words('english'))
@@ -7,8 +8,8 @@ english_stopwords.add('')
 hindi_stopwords = set([u'hai', u'ki', u'ho', u'ko', u'ke', u'ka', u'h', u'se', u'bhi', u'hi', u'aap', u'u', u'k', u'ye', u'aur', u'p', u'tha', u'kya', u'kar', u'ji', u'd', u'ek', u'koi', u'nhi', u'mein', u'ne', u'pe', u'na', u'toh', u'kuch', u'ab', u'jo', u'httpURL', u'sab', u'par', u'hain', u'rt', u'b', u'2', u'tu', u'mai', u'hum', u'thi', u'main', u'apne', u'ni', u'kr', u'yaar', u'im', u'ha', u'wo', u'aa', u'v', u'hu', u'de', u'ap', u'amp', u'3', u'ur', u'r', u'ya', u'n', u'its', u'4', u'1', u'hua', u'1st', u'gya', u'yeh', u'le', u'apni'])
 stop_words = hindi_stopwords|english_stopwords
 
-print stop_words
-print len(stop_words)
+# print stop_words
+# print len(stop_words)
 
 filename = '../../refinedData/final_codemix_data.json'
 open_file = open(filename, 'r')
@@ -30,21 +31,52 @@ for tweet in data_read:
 	l = []
 	for i in range(len(text)):
 		# l.append((text[i], tags[i]))
-		l.append(text[i])
+		if validators.url(text[i]) != True:
+			l.append(text[i].lower())
 	data.append(l)
 	c+=1
-print data
+
+def clean(doc):
+	# print doc
+	stop_free = " ".join([i for i in doc if i.lower() not in stop_words])
+	punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+	return punc_free
+data = [clean(doc).split() for doc in data]
 
 words = []
 counts = []
 for sentence in data:
 	for word in sentence:
 		if word not in words:
-			words.append(word)
-			counts.append(0)
-		if word in words:
+			if word not in stop_words:
+				words.append(word)
+				counts.append(1)
+		else:
 			index = words.index(word)
 			counts[index]+=1
 print len(words)
 print len(counts)
-		
+
+words_and_counts = zip(counts, words)
+words_and_counts.sort()
+words_and_counts = words_and_counts[::-1]
+# print words_and_counts
+print len(words_and_counts)
+
+words_with_num = []
+for count in range(len(words_and_counts)):
+	words_with_num.append((count+1, words_and_counts[count][1]))
+# Top 6000 words
+words_with_num = words_with_num[:6000]
+
+dictionary = {}
+words_to_num = {}
+num_to_words = {}
+
+for word in words_with_num:
+	words_to_num[word[1]] = word[0]
+	num_to_words[word[0]] = word[1]
+
+dictionary['words_to_num'] = words_to_num
+dictionary['num_to_words'] = num_to_words
+
